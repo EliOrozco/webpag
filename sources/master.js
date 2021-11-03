6c2f6d944acfe4d31b2179a25d39feb7077d9e1e
EliOrozco <92065409+EliOrozco@users.noreply.github.com>
2021-11-02 22:46:23 -0600

    ye
    cambios

---
+++ sources/master.js
@@ -0,0 +1,16 @@
+function colapsarMenu() {
+  var x = document.getElementById("demo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+function acordeon(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
